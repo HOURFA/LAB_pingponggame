79923129cb88dd702f592506115cbb561ea4cbc5
LAPTOP-HUPKH7T3\RFA <C109112113@nkust.edu.tw>
2022-11-02 20:56:28 +0800

    2022.11.02 :課堂進度 備註 : (1)需優化程式   (2)已可正常運作，但仍有bug

--- project_1.srcs/sources_1/new/counter.vhd
+++ project_1.srcs/sources_1/new/counter.vhd
@@ -20,71 +20,71 @@ begin
 process(rst,clk1,clr_a,clr_b,hit,led_loc)
 begin
 if rst = '1' then
-	if clk1 = '1' and clk1 'event then
-		case hit is
-			when sa  => ps	<= '0';
-					    if clr_a = '1' then--a發球  
-					        ps	<= '1';
-					        led_act	<= "001";
-					        hit <= hb;                                                                	                           
-					    elsif clr_a = '0' then hit <= sa;
-					    end if;
-			when sb  => ps	<= '1';    
-						if clr_b = '1' then --b發球 
-							ps	<= '0'; 
-							led_act	<= "000";                                                       	                                                             	                               
-							hit <= ha;                                                               
-						elsif clr_b = '0' then hit <= sb;
-						end if;
-			when ha  => case clr_a is 
-							when '1' => if led_loc = "0111" then--正確回擊
-											led_act  <= "011";
-											hit <= hb;
-										else --錯誤回擊
-											led_act 		<= "100";
-											point_b <= point_b +1 ;
-											hit 	<= j; 
-										end if;
-							when '0' => case led_loc is    	                                               
-											when "0111" =>     --沒有打到球
-												point_b <= point_b +1 ;
-												led_act 		<= "100";                                                 
-												hit  	<= j;
-											when others =>hit <= ha;
-										end case;
-							when others =>hit <= ha;
-						end case;
-			when hb  => case clr_b is 
-							when '1' => if led_loc = "0000" then--正確回擊
-											led_act  <= "010";
-											hit <= ha;
-										else --錯誤回擊
-											led_act 		<= "100";
-											point_a <= point_a +1;
-											hit 	<= j; 
-										end if;
-							when '0' => case led_loc is 
-											when "0000" =>
-												point_a <= point_a +1;
-												led_act 		<= "100";                                                
-												hit  	<= j;
-											when others =>hit <= hb;
-										end case;                                                    
-							when others =>hit	<= hb;                                 
-						end case;                                                         
-			when j   =>case ps is 
-							when '0' 	=> hit	<= sa;    	                                       
-							when '1' 	=> hit	<= sb;
-							when others => NULL;
-						end case;
-		end case;
-	end if;
-else
 	hit 	<= sa;
 	ps 		<= '0';       
 	point_a <= (others => '0');--右邊
-	point_b <= (others => '0');--左邊
-end if;
+	point_b <= (others => '0');--左邊	
+else
+    if clk1 = '1' and clk1 'event then
+            case hit is
+                when sa  => ps	<= '0';
+                            if clr_a = '1' then--a發球  
+                                ps	<= '1';
+                                led_act	<= "001";
+                                hit <= hb;                                                                	                           
+                            elsif clr_a = '0' then hit <= sa;
+                            end if;
+                when sb  => ps	<= '1';    
+                            if clr_b = '1' then --b發球 
+                                ps	<= '0'; 
+                                led_act	<= "000";                                                       	                                                             	                               
+                                hit <= ha;                                                               
+                            elsif clr_b = '0' then hit <= sb;
+                            end if;
+                when ha  => case clr_a is 
+                                when '1' => if led_loc = "0111" then--正確回擊
+                                                led_act  <= "011";
+                                                hit <= hb;
+                                            else --錯誤回擊
+                                                led_act 		<= "100";
+                                                point_b <= point_b +1 ;
+                                                hit 	<= j; 
+                                            end if;
+                                when '0' => case led_loc is    	                                               
+                                                when "0111" =>     --沒有打到球
+                                                    point_b <= point_b +1 ;
+                                                    led_act 		<= "100";                                                 
+                                                    hit  	<= j;
+                                                when others =>hit <= ha;
+                                            end case;
+                                when others =>hit <= ha;
+                            end case;
+                when hb  => case clr_b is 
+                                when '1' => if led_loc = "0000" then--正確回擊
+                                                led_act  <= "010";
+                                                hit <= ha;
+                                            else --錯誤回擊
+                                                led_act 		<= "100";
+                                                point_a <= point_a +1;
+                                                hit 	<= j; 
+                                            end if;
+                                when '0' => case led_loc is 
+                                                when "0000" =>
+                                                    point_a <= point_a +1;
+                                                    led_act 		<= "100";                                                
+                                                    hit  	<= j;
+                                                when others =>hit <= hb;
+                                            end case;                                                    
+                                when others =>hit	<= hb;                                 
+                            end case;                                                         
+                when j   => case ps is 
+                                when '0' 	=> hit	<= sa;    	                                       
+                                when '1' 	=> hit	<= sb;
+                                when others => NULL;
+                            end case;
+            end case;
+        end if;
+    end if;
 end process;
 prestate    <= ps;
 score_a 	<= point_a;
